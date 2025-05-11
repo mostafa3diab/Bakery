02000d70ecb75d108663fdd05d00741add8c3a81
MostafaDiab <mostafa3diab55@gmail.com>
2025-05-11 19:39:14 +0300

    fixed the reload page issue for the Navbar

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,9 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export let userContext = createContext();
 
 export default function UserContextProvider(props) {
   const [isLogin, setLogin] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("userToken");
+    if (token) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  }, []);
 
   return (
     <userContext.Provider value={{ isLogin, setLogin }}>
